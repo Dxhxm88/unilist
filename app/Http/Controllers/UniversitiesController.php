1e916f2d584277f757b8ab0880e9eeacf9df149c
Edham Rabuan <edhamrabuan@Haniss-MacBook.local>
2022-04-09 00:40:06 +0800

    first commit

---
+++ app/Http/Controllers/UniversitiesController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Universities;
+use Illuminate\Http\Request;
+
+class UniversitiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Universities  $universities
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Universities $universities)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Universities  $universities
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Universities $universities)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Universities  $universities
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Universities $universities)
+    {
+        //
+    }
+}
